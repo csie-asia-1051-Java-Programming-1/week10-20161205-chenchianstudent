0b83304f080a4f781c4e26d811aff16f0d5e3775
chenchianstudent <105021007@live.asia.edu.tw>
2016-12-05 16:37:48 +0800

    3

--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 /*
  * Topic: 輸入所使用的度數，換算夏月及非夏月之電費金額。
  * Date: 2016/12/05
- * Author: 1050210XX 周永振老師
+ * Author: 105021007 陳麒安
  */
 
 public class ex02 {
--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,10 +1,57 @@
 package hw;
+import java.util.Scanner;
 
 public class hw02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
-
+Scanner scn=new Scanner(System.in);
+System.out.print("請輸入4個數");
+int n1=scn.nextInt();
+int n2=scn.nextInt();
+int n3=scn.nextInt();
+int n4=scn.nextInt();
+if((n1==n2&&n1==n3&&n1==n4)||(n2==n1&&n2==n3&&n2==n4)||(n3==n2&&n3==n1&&n3==n4)||(n4==n2&&n4==n3&&n4==n1)){
+System.out.print("通殺");	
+}else{
+if((n1==n2&&n1==n3)||(n1==n2&&n1==n4)||(n1==n3&&n1==n4)){
+System.out.print(n1*2);	
+}else{
+if((n2==n1&&n2==n3)||(n2==n1&&n2==n4)||(n2==n3&&n2==n4)){
+System.out.print(n2*2);	
+}else{
+if((n3==n1&&n3==n2)||(n3==n1&&n3==n4)||(n3==n2&&n3==n4)){
+System.out.print(n3*2);	
+}else{
+if((n4==n1&&n4==n2)||(n4==n1&&n4==n3)||(n4==n2&&n4==n3)){
+System.out.print(n4*2);	
+}else{
+if((n1==n2)||(n1==n3)||(n1==n4)){
+System.out.print(n1*2);
+}else{
+if((n2==n1)||(n2==n3)||(n2==n4)){
+System.out.print(n2*2);	
+}else{
+if((n3==n1)||(n3==n2)||(n3==n4)){
+System.out.print(n3*2);	
+}else{
+if((n4==n1)||(n4==n2)||(n4==n3)){
+System.out.print(n4*2);	
+}else{
+System.out.print("R");	
+}	
+}	
+}	
+}	
+}	
+}	
+}	
+}	
 }
+	}	
+}
+	
+
+
+
+
